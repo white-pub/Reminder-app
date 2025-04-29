bd44b285af9ed2f7cd4b065fbcc77146965ca1c7
Lin2025-888 <s96024n@gmail.com>
2025-04-28 23:27:16 -0500

    created a test for views ReminderViewSet

--- backend/reminders/tests.py
+++ backend/reminders/tests.py
@@ -11,6 +11,7 @@ from reminders.models import Reminder
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class TestReminderModel(TestCase):
@@ -59,3 +60,53 @@ class TestSignupAPI(APITestCase):
         # check response message and database
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('error', response.data)
+
+
+class TestReminderViewSet(APITestCase):
+    def setUp(self):
+        # Create user1 for testing
+        self.user1 = User.objects.create_user(username="user1", password="Password1")
+
+        # Authenticate user to ensure access
+        refresh = RefreshToken.for_user(self.user1)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+
+        # Create reminders for user1
+        Reminder.objects.create(
+            title="User 1 Reminder 1", user=self.user1, remind_time="2025-01-01T10:00:00Z"
+        )
+        Reminder.objects.create(
+            title="User 1 Reminder 2", user=self.user1, remind_time="2025-02-01T10:00:00Z"
+        )
+
+        # Log in as user1
+        self.client.login(username="user1", password="password1")
+
+    def test_get_queryset(self):
+        # Test that user1 can access their own reminders
+        response = self.client.get("/api/reminders/")
+
+        # user1 and its 2 reminders
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)  
+        titles = [reminder["title"] for reminder in response.data]
+        self.assertIn("User 1 Reminder 1", titles)
+        self.assertIn("User 1 Reminder 2", titles)
+
+    def test_create_reminder_saves_user_as_owner(self):
+        # Test that a new reminder is saved with the authenticated user as the owner
+        
+        # Create the new reminder entry
+        data = {
+            "title": "New Reminder",
+            "remind_time": "2025-03-01T10:00:00Z",
+            "description": "Test reminder",
+        }
+
+        # send to the api
+        response = self.client.post("/api/reminders/", data)
+        # check response message and database
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(
+            Reminder.objects.filter(title="New Reminder", user=self.user1).exists()
+        )
